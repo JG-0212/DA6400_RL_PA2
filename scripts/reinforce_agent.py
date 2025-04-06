3c5fd7490d89e2b6826ca7cac5b9ba5a703d67c1
Lalit <jayanti.lalit@gmail.com>
2025-04-06 10:08:07 +0530

    added baseline

--- scripts/reinforce_agent.py
+++ scripts/reinforce_agent.py
@@ -36,7 +36,7 @@ class PolicyNetwork(nn.Module):
 
 class ValueNetwork(nn.Module):
 
-    def __init__(self, state_size, action_size, seed):
+    def __init__(self, state_size, seed):
         super().__init__()
 
         self.seed = torch.manual_seed(seed)
@@ -188,7 +188,6 @@ class ReinforceMCwithBaselineAgent:
                                             self.action_size,
                                             seed).to(self.device)
         self.value_network = ValueNetwork(self.state_size,
-                                          self.action_size,
                                           seed).to(self.device)
         self.policy_optimizer = optim.Adam(
             self.policy_network.parameters(), lr=self.LR_POLICY
@@ -217,28 +216,43 @@ class ReinforceMCwithBaselineAgent:
         discounted_returns.reverse()
 
         for step, G in zip(self.episode_history, discounted_returns):
-            state, action, reward = step.state, step.action, step.reward
+            state, action, reward, next_state = step.state, step.action, step.reward, step.next_state
             state = torch.tensor(state, dtype=torch.float32,
+                                 device=self.device).unsqueeze(0)
+            next_state = torch.tensor(next_state, dtype=torch.float32,
                                  device=self.device).unsqueeze(0)
             G = torch.tensor([G], dtype=torch.float32,
                              device=self.device).unsqueeze(0)
 
+            state_value = self.value_network(state)
+            next_state_value = self.value_network(next_state)
             action_probs = self.policy_network(state)
+
             action_dist = Categorical(action_probs)
             log_prob = action_dist.log_prob(torch.tensor(
                 action, dtype=torch.long, device=self.device))
 
-            policy_loss = -log_prob*G
+            # value_loss = F.mse_loss(state_value, G)
+            value_loss = F.mse_loss(
+                state_value,
+                reward + self.GAMMA*next_state_value
+            )
 
-            state_value = self.value_network(state)
-            value_loss = F.mse_loss(state_value, G)
+            baseline = state_value.detach()
+            policy_loss = -log_prob*(G - baseline)
 
             self.value_optimizer.zero_grad()
             value_loss.backward()
+            for param in self.value_network.parameters():
+                param.grad.data.clamp_(-1, 1)
+
             self.value_optimizer.step()
 
             self.policy_optimizer.zero_grad()
             policy_loss.backward()
+            for param in self.policy_network.parameters():
+                param.grad.data.clamp_(-1, 1)
+
             self.policy_optimizer.step()
 
         self.episode_history.clear()
