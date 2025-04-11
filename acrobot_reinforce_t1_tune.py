5f4d07233d70081b5f7750a7c7acf76d8302e58a
Lalit <jayanti.lalit@gmail.com>
2025-04-11 21:32:19 +0530

    acrobot wandb tuning

---
+++ acrobot_reinforce_t1_tune.py
@@ -0,0 +1,82 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import wandb
+import gymnasium as gym
+from gymnasium.wrappers import RecordVideo
+
+from scripts.ddqn_agent import DDQNAgent
+from scripts.reinforce_agent import ReinforceMCwithoutBaselineAgent, ReinforceMCwithBaselineAgent
+from scripts.training import Trainer, trainingInspector, test_agent, plot_test_results, compute_decay
+
+
+def episode_trigger(x):
+    if x % 200 == 0:
+        return True
+    return False
+
+def main():
+    """Function setup to configure a sweep run, record videos of policy in action and
+    log results in wandb
+    """
+    run = wandb.init()
+    config = wandb.config
+
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = 'cpu'
+    num_episodes = 2000
+    max_return = -100
+    reinforce_type1_hyperparameter_list = [
+        {
+            "num_episodes": num_episodes,
+            "max_return": max_return,
+            "LR_POLICY": float(config.learning_rate_policy),
+        }
+    ]
+
+    run.name = repr(reinforce_type1_hyperparameter_list[0]).strip("{}")
+
+    env = gym.make('Acrobot-v1', render_mode="rgb_array")
+    env = RecordVideo(
+        env,
+        video_folder="backups/acrobot-reinforce-type1-visualizations",
+        name_prefix="eval",
+        episode_trigger=episode_trigger
+    )
+
+
+    reinforce_type1_agent = ReinforceMCwithoutBaselineAgent(
+        state_space=env.observation_space,
+        action_space=env.action_space,
+        device=device,
+        seed=0
+    )
+
+    trainer = Trainer()
+    reinforce_type1_results = test_agent(
+        env, reinforce_type1_agent, trainer, reinforce_type1_hyperparameter_list, num_experiments=3)
+
+    env.close()
+
+    for score in reinforce_type1_results[0]["means"]:
+        wandb.log(
+            {
+                "score": score
+            }
+        )
+    for moving_avg in reinforce_type1_results[0]["rolling_means"]:
+        wandb.log(
+            {
+                "mean_score": moving_avg
+            }
+        )
+
+    wandb.log({
+        "max_mean_score": np.max(reinforce_type1_results[0]["rolling_means"]),
+        "regret": num_episodes*max_return - np.sum(reinforce_type1_results[0]["means"])
+    })
+
+
+if __name__ == '__main__':
+    main()
